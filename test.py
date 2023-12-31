95edc93a50dbd87c5b84c8393e991205a66509d4
jiwoo219 <wldn3200@gmail.com>
2023-12-31 12:55:03 +0900

    second commit

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from pybithumb import Bithumb
 import time
 import datetime
 
-bithumb = Bithumb("06b42812e4aaec48580d555510727836", "a67680d16e15140a872911b6728e0be9")
+bithumb = Bithumb("e94e5052bc5971b42f885a175507c5f9", "53582a67fc5d830641e5e8c1edb332b0")
 
 
 # 코인 리스트 가져오기
 # coins = Bithumb.get_tickers()
-coins = ['BSV']
+coins = ['BTC']
 
 # 내 자산현황 가져오기
 # for coin in coins:
@@ -19,30 +19,34 @@ last_reset_time = datetime.datetime.now()
 total = 0
 
 # 자동 거래 시작
-while total < 15000000000:
+while total < 1500000000:
     try:
         for coin in coins:
             # 코인의 현재 잔고 조회
             balance = bithumb.get_balance(coin)
-            print(balance)
 
-            if balance[0] > 0.00001 and balance[1] < 0.00001:
+            if balance[0] > 0.000001 and balance[1] < 0.000001:
                 # 매도
-                ask_price = Bithumb.get_orderbook(coin)['asks'][2]['price'] # 제 2 매도호가
+                ask_price = Bithumb.get_orderbook(coin)['asks'][1]['price'] # 제 2 매도호가
                 desc = bithumb.sell_limit_order(coin, ask_price, balance[0])
+                total += ask_price * balance[0]
                 print(desc)
 
-                # 매수와 매도 사이에 적절한 대기 시간 설정
                 time.sleep(10)
+                print("total", total)
 
-            if balance[0] < 0.00001 and balance[3] < 0.00001:
+            if balance[0] < 0.000001 and balance[3] < 0.000001:
                 # 매수
-                ask_price = Bithumb.get_orderbook(coin)['bids'][2]['price'] # 제 2 매수호가
-                if balance[2] < 470000:
+                bid_price = Bithumb.get_orderbook(coin)['bids'][1]['price'] # 제 2 매수호가
+                if balance[2] < 400000:
                     total = 16000000000
 
-                desc = bithumb.buy_limit_order(coin, ask_price, balance[2] / ask_price)
+                desc = bithumb.buy_limit_order(coin, bid_price, balance[2] / bid_price * 0.99)
+                total += balance[2] * 0.85
                 print(desc)
+
+                time.sleep(10)
+                print("total", total)
 
             # 24시간이 경과하면 total을 0으로 초기화
             current_time = datetime.datetime.now()
